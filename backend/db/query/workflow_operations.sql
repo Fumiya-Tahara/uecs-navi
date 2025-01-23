1ba4c01e9b6792f0ebb8572df88ac3b31c3a9ad1
Fumiya Tahara <taharafumiya0219@gmail.com>
2025-01-23 09:29:20 +0000

    Fix workflow operation queries

--- backend/db/query/workflow_operations.sql
+++ backend/db/query/workflow_operations.sql
@@ -2,7 +2,7 @@
 INSERT INTO workflow_operations (workflow_id, relay_1, relay_2, relay_3, relay_4, relay_5, relay_6, relay_7, relay_8) 
 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
 
--- name: GetWorkflowOperationsFromWorkflow :many
+-- name: GetWorkflowOperationsFromWorkflow :one
 SELECT id, workflow_id, relay_1, relay_2, relay_3, relay_4, relay_5, relay_6, relay_7, relay_8
 FROM workflow_operations
 WHERE workflow_id = ?;
