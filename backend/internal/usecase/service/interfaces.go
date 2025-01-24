d5a39e4b7c590e29daa9425841fe8f0d989a15a3
Fumiya Tahara <taharafumiya0219@gmail.com>
2025-01-24 08:52:23 +0000

    Add repository interfaces

--- backend/internal/usecase/service/interfaces.go
+++ backend/internal/usecase/service/interfaces.go
@@ -2,59 +2,67 @@ package service
 
 import (
 	"github.com/Fumiya-Tahara/uecs-navi.git/internal/domain"
-	"github.com/Fumiya-Tahara/uecs-navi.git/internal/usecase/repository"
 )
 
 type (
 	// Repository interfaces
-	DeviceRepositoryInterface interface {
-		CreateDevice(newDevice domain.Device) (int64, error)
-		GetDevicesFromHouse(houseID int) ([]*domain.Device, error)
-		GetJoinedDevicesFromHouse(houseID int) ([]*repository.JoinedDevice, error)
-		GetDeviceFromID(deviceID int) (*domain.Device, error)
-	}
-
-	HouseRepositoryInterface interface {
-		CreateHouse(name string) (int64, error)
-		GetAllHouses() ([]*domain.House, error)
-	}
-
 	ClimateDataRepositoryInterface interface {
 		GetAllClimateData() ([]*domain.ClimateData, error)
 		GetClimateDataFromID(ID int) (*domain.ClimateData, error)
 	}
 
-	SensorRepositoryInterface interface {
-		CreateSensor(newSensor domain.Sensor) (int64, error)
-		GetAllSensor() ([]*domain.Sensor, error)
-		GetSensorFromID(ID int) (*domain.Sensor, error)
+	ComparisonOperatorRepositoryInterface interface {
+		GetAllComparisonOperators() ([]*domain.ComparisonOperator, error)
+		GetAllComparisonOperatorFromID(ID int) *domain.ComparisonOperator
+	}
+
+	ConditionRepositoryInterface interface {
+		CreateCondition(newCondition *domain.Condition) (int, error)
+		GetConditionFromTimeScheduleRow(timeScheduleRowID int) (*domain.Condition, error)
+	}
+
+	DeviceRepositoryInterface interface {
+		CreateDevice(newDevice domain.Device) (int, error)
+		GetDevicesFromM304(m304ID int) ([]*domain.Device, error)
+	}
+
+	EdgeRepositoryInterface interface {
+		CreateEdge(newEdge domain.Edge) (int, error)
+		GetEdgesFromWorkflow(workflowID int) ([]*domain.Edge, error)
+	}
+
+	HouseRepositoryInterface interface {
+		CreateHouse(name string) (int, error)
+		GetAllHouses() ([]*domain.House, error)
 	}
 
 	M304RepositoryInterface interface {
-		CreateM304(newM304 domain.M304) (int64, error)
+		CreateM304(newM304 domain.M304) (int, error)
 		GetM304FromID(ID int) (*domain.M304, error)
 	}
 
-	M304RecordRepositoryInterface interface {
-		CreateM304Record(NewM304Record domain.M304Record) (int64, error)
-		GetM304RecordFromM304ID(m304ID int) ([]*domain.M304Record, error)
+	NodeRepositoryInterface interface {
+		CreateNode(newNode domain.Node) (int, error)
+		GetNodesFromWorkflow(workflowID int) ([]*domain.Node, error)
 	}
 
-	DeviceConditionRepositoryInterface interface {
-		CreateDeviceCondition(newDeviceCondition domain.DeviceCondition) (int64, error)
-		GetDeviceConditionFromID(ID int) (*domain.DeviceCondition, error)
-		GetDeviceConditionsFromDeviceID(deviceID int) ([]*domain.DeviceCondition, error)
-	}
-
-	OperationRepositoryInterface interface {
-		CreateOperation(newOperation domain.Operation) (int64, error)
-		GetOperationFromID(ID int) (*domain.Operation, error)
-		GetOperationsFromDeviceID(deviceID int) ([]*domain.Operation, error)
+	TimeScheduleRowRepositoryInterface interface {
+		CreateTimeScheduleRow(newTimeScheduleRow domain.TimeScheduleRow) (int, error)
+		GetTimeScheduleRowsFromTimeSchedule(timeScheduleID int) ([]*domain.TimeScheduleRow, error)
 	}
 
 	TimeScheduleRepositoryInterface interface {
-		CreateTimeSchedule(newTimeSchedule domain.TimeSchedule) (int64, error)
-		GetTimeScheduleFromID(ID int) (*domain.TimeSchedule, error)
-		GetTimeSchedulesFromDeviceCondition(deviceConditionID int) ([]*domain.TimeSchedule, error)
+		CreateTimeSchedule(m304ID int) (int, error)
+		GetTimeScheduleFromM304(m304ID int, rows []domain.TimeScheduleRow) (*domain.TimeSchedule, error)
+	}
+
+	WorkflowOperationRepositoryInterface interface {
+		CreateWorkflowOperation(newWorkflowOperation domain.WorkflowOperation) (int, error)
+		GetWorkflowOperationsFromWorkflow(workflowID int) (*domain.WorkflowOperation, error)
+	}
+
+	WorkflowRepositoryInterface interface {
+		CreateWorkflow(newWorkflow domain.Workflow) (int, error)
+		GetWorkflowsFromM304(m304ID int) ([]*domain.Workflow, error)
 	}
 )
