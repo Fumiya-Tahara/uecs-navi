acdcaf56e2ff7b1d872e5bbecb846f97ba50dcc8
Fumiya Tahara <taharafumiya0219@gmail.com>
2025-02-06 09:09:45 +0000

    Fix the time schedule converter to include workflows

--- backend/internal/controller/dto/time_schedule_response.go
+++ backend/internal/controller/dto/time_schedule_response.go
@@ -1,15 +1,17 @@
 package dto
 
 type TimeScheduleResponse struct {
-	ID               int
+	ID               int               `json:"id"`
 	M304ID           int               `json:"m304_id"`
+	Workflows        []Workflow        `json:"workflows"`
 	TimeScheduleRows []TimeScheduleRow `json:"time_schedules"`
 }
 
-func NewTimeScheduleResponse(id, m304ID int, timeSchedules []TimeScheduleRow) *TimeScheduleResponse {
+func NewTimeScheduleResponse(id, m304ID int, workflows []Workflow, timeSchedules []TimeScheduleRow) *TimeScheduleResponse {
 	return &TimeScheduleResponse{
 		ID:               id,
 		M304ID:           m304ID,
+		Workflows:        workflows,
 		TimeScheduleRows: timeSchedules,
 	}
 }
