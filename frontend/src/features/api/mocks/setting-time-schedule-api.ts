3acf4f67f8a0c448637ad8943a36a61c7ed5baf6
Fumiya Tahara <taharafumiya0219@gmail.com>
2025-03-01 08:13:08 +0000

    Fix time  schedule page mock functions

--- frontend/src/features/api/mocks/setting-time-schedule-api.ts
+++ frontend/src/features/api/mocks/setting-time-schedule-api.ts
@@ -1,213 +1,191 @@
-import {
-  TimeScheduleResponse,
-  WorkflowResponse,
-  ClimateDataResponse,
-} from "@/types/api";
+import { TimeScheduleResponse, ClimateData } from "@/types/api";
 
-export function getTimeSchedules(houseID: number): TimeScheduleResponse[] {
-  const nasuTimeSchedules: TimeScheduleResponse[] = [
-    {
-      start_time: "00:00",
-      end_time: "02:30",
-      workflows: [
-        {
-          id: 1,
-          name: "高めの加温",
+export function getTimeSchedules(m304ID: number): TimeScheduleResponse | null {
+  const schedule1: TimeScheduleResponse = {
+    id: 1,
+    m304_id: 1,
+    workflows: [
+      {
+        id: 1,
+        m304_id: 1,
+        name: "高めの加温",
+      },
+      {
+        id: 2,
+        m304_id: 1,
+        name: "加湿",
+      },
+    ],
+    time_schedules: [
+      {
+        start_time: "03:00",
+        end_time: "05:00",
+        selected_workflow_id: 1,
+        condition: {
+          selected_climate_data_id: 1,
+          selected_comparison_operator_id: 1,
+          set_point: 1,
         },
-        {
-          id: 2,
-          name: "加湿",
+      },
+      {
+        start_time: "05:30",
+        end_time: "07:30",
+        selected_workflow_id: 1,
+        condition: {
+          selected_climate_data_id: 1,
+          selected_comparison_operator_id: 1,
+          set_point: 1,
         },
-      ],
-    },
-    {
-      start_time: "03:00",
-      end_time: "05:00",
-      workflows: [
-        {
-          id: 1,
-          name: "高めの加温",
+      },
+      {
+        start_time: "08:00",
+        end_time: "10:00",
+        selected_workflow_id: 1,
+        condition: {
+          selected_climate_data_id: 1,
+          selected_comparison_operator_id: 1,
+          set_point: 1,
         },
-        {
-          id: 3,
-          name: "CO2供給",
+      },
+      {
+        start_time: "10:30",
+        end_time: "12:30",
+        selected_workflow_id: 1,
+        condition: {
+          selected_climate_data_id: 1,
+          selected_comparison_operator_id: 1,
+          set_point: 1,
         },
-      ],
-    },
-    {
-      start_time: "05:30",
-      end_time: "07:30",
-      workflows: [
-        {
-          id: 2,
-          name: "加湿",
+      },
+      {
+        start_time: "13:00",
+        end_time: "15:00",
+        selected_workflow_id: 1,
+        condition: {
+          selected_climate_data_id: 1,
+          selected_comparison_operator_id: 1,
+          set_point: 1,
         },
-        {
-          id: 4,
-          name: "換気",
-        },
-      ],
-    },
-    {
-      start_time: "08:00",
-      end_time: "10:00",
-      workflows: [
-        {
-          id: 1,
-          name: "高めの加温",
-        },
-        {
-          id: 5,
-          name: "照明",
-        },
-      ],
-    },
-    {
-      start_time: "10:30",
-      end_time: "12:30",
-      workflows: [
-        {
-          id: 3,
-          name: "CO2供給",
-        },
-        {
-          id: 4,
-          name: "換気",
-        },
-      ],
-    },
-    {
-      start_time: "13:00",
-      end_time: "15:00",
-      workflows: [
-        {
-          id: 2,
-          name: "加湿",
-        },
-        {
-          id: 5,
-          name: "照明",
-        },
-      ],
-    },
-  ];
+      },
+    ],
+  };
 
-  const hourensouTimeSchedule: TimeScheduleResponse[] = [
-    {
-      start_time: "06:00",
-      end_time: "08:00",
-      workflows: [
-        {
-          id: 1,
-          name: "加温",
+  const schedule2: TimeScheduleResponse = {
+    id: 2,
+    m304_id: 2,
+    workflows: [
+      {
+        id: 3,
+        m304_id: 2,
+        name: "高めの加温",
+      },
+      {
+        id: 4,
+        m304_id: 2,
+        name: "加湿",
+      },
+    ],
+    time_schedules: [
+      {
+        start_time: "03:00",
+        end_time: "05:00",
+        selected_workflow_id: 3,
+        condition: {
+          selected_climate_data_id: 1,
+          selected_comparison_operator_id: 1,
+          set_point: 1,
         },
-        {
-          id: 2,
-          name: "加湿",
+      },
+      {
+        start_time: "05:30",
+        end_time: "07:30",
+        selected_workflow_id: 3,
+        condition: {
+          selected_climate_data_id: 1,
+          selected_comparison_operator_id: 1,
+          set_point: 1,
         },
-      ],
-    },
-    {
-      start_time: "08:30",
-      end_time: "10:30",
-      workflows: [
-        {
-          id: 3,
-          name: "CO2供給",
+      },
+      {
+        start_time: "08:00",
+        end_time: "10:00",
+        selected_workflow_id: 3,
+        condition: {
+          selected_climate_data_id: 1,
+          selected_comparison_operator_id: 1,
+          set_point: 1,
         },
-        {
-          id: 4,
-          name: "換気",
+      },
+      {
+        start_time: "10:30",
+        end_time: "12:30",
+        selected_workflow_id: 3,
+        condition: {
+          selected_climate_data_id: 1,
+          selected_comparison_operator_id: 1,
+          set_point: 1,
         },
-      ],
-    },
-    {
-      start_time: "11:00",
-      end_time: "13:00",
-      workflows: [
-        {
-          id: 1,
-          name: "加温",
+      },
+      {
+        start_time: "13:00",
+        end_time: "15:00",
+        selected_workflow_id: 3,
+        condition: {
+          selected_climate_data_id: 1,
+          selected_comparison_operator_id: 1,
+          set_point: 1,
         },
-        {
-          id: 5,
-          name: "照明",
-        },
-      ],
-    },
-    {
-      start_time: "13:30",
-      end_time: "15:30",
-      workflows: [
-        {
-          id: 2,
-          name: "加湿",
-        },
-        {
-          id: 4,
-          name: "換気",
-        },
-      ],
-    },
-    {
-      start_time: "16:00",
-      end_time: "18:00",
-      workflows: [
-        {
-          id: 3,
-          name: "CO2供給",
-        },
-        {
-          id: 5,
-          name: "照明",
-        },
-      ],
-    },
-  ];
+      },
+    ],
+  };
 
-  const timeScheduleMap: Map<number, TimeScheduleResponse[]> = new Map();
+  const schedule3: TimeScheduleResponse = {
+    id: 3,
+    m304_id: 3,
+    workflows: [
+      {
+        id: 1,
+        m304_id: 5,
+        name: "高めの加温",
+      },
+      {
+        id: 2,
+        m304_id: 6,
+        name: "加湿",
+      },
+    ],
+    time_schedules: [
+      {
+        start_time: "03:00",
+        end_time: "05:00",
+        selected_workflow_id: 5,
+        condition: {
+          selected_climate_data_id: 1,
+          selected_comparison_operator_id: 1,
+          set_point: 1,
+        },
+      },
+    ],
+  };
 
-  timeScheduleMap.set(2, nasuTimeSchedules);
-  timeScheduleMap.set(3, hourensouTimeSchedule);
+  const timeScheduleMap: Map<number, TimeScheduleResponse> = new Map();
 
-  const result: TimeScheduleResponse[] = timeScheduleMap.get(houseID) || [];
+  timeScheduleMap.set(1, schedule1);
+  timeScheduleMap.set(2, schedule2);
+  timeScheduleMap.set(3, schedule3);
+
+  const result: TimeScheduleResponse | null =
+    timeScheduleMap.get(m304ID) || null;
 
   return result;
 }
 
-export function getWorkflows(): WorkflowResponse[] {
-  const workflows: WorkflowResponse[] = [
-    {
-      id: 1,
-      name: "加温",
-    },
-    {
-      id: 2,
-      name: "加湿",
-    },
-    {
-      id: 3,
-      name: "CO2供給",
-    },
-    {
-      id: 4,
-      name: "換気",
-    },
-    {
-      id: 5,
-      name: "照明",
-    },
-  ];
-
-  return workflows;
-}
-
-export function getClimateDatas(): ClimateDataResponse[] {
-  const climateDatas: ClimateDataResponse[] = [
-    { id: 1, climate_data: "気温", unit: "℃" },
-    { id: 2, climate_data: "湿度", unit: "%" },
-    { id: 3, climate_data: "二酸化炭素量", unit: "ppm" },
-    { id: 4, climate_data: "照度", unit: "lx" },
+export function getClimateDatas(): ClimateData[] {
+  const climateDatas: ClimateData[] = [
+    { id: 1, name: "気温", unit: "℃" },
+    { id: 2, name: "湿度", unit: "%" },
+    { id: 3, name: "二酸化炭素量", unit: "ppm" },
+    { id: 4, name: "照度", unit: "lx" },
   ];
 
   return climateDatas;
