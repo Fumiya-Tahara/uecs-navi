0b3cdb0eb526319e08933bf2b10e787d42090c5a
Fumiya Tahara <taharafumiya0219@gmail.com>
2025-03-09 05:47:48 +0000

    Add geting all M304s to sql

--- backend/db/query/m304s.sql
+++ backend/db/query/m304s.sql
@@ -2,6 +2,9 @@
 INSERT INTO m304s (house_id, mac_addr, dhcp_flg, ip_addr, net_mask, defgw, dns, vender_name, node_name)
 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
 
+-- name: GetAllM304s :many
+SELECT * FROM m304s;
+
 -- name: GetM304FromID :one
 SELECT id, house_id, mac_addr, dhcp_flg, ip_addr, net_mask, defgw, dns, vender_name, node_name
 FROM m304s
