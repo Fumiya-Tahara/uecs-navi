39d53940dbbe0560f72cbac5cf46d8ed884265a1
Fumiya Tahara <taharafumiya0219@gmail.com>
2025-05-11 08:06:36 +0000

    Modify next settings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Outputs a Single-Page Application (SPA).
-  distDir: "./dist", // Changes the build output directory to `./dist/`.
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
